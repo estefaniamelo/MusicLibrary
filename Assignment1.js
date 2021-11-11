3929710c1ddd70f09182f288ea153019e4d40c47
estefaniamelo <melojoa@sheridancollege.ca>
2021-11-11 17:31:31 -0500

    addAlbumToLibrary function storing album objects to array

--- Assignment1.js
+++ Assignment1.js
@@ -5,19 +5,40 @@
  * 
  * Your code should look something like this:
  */
+    //declaring array
     let myLibrary = [];
-    
-    //adding books to the library
 
 
-    function Album(){
-        //the constructor
+    //adding event listener to addAlbum button
+   
+
+    function Album(albumName, artist, songs, listenedStatus){
+        this.albumName = albumName;
+        this.artist = artist;
+        this.songs = songs;
+        this.listenedStatus = listenedStatus;
     }
 
+    document.getElementById("addAlbum").addEventListener("click", addAlbumToLibrary);
+
+    //function to add a new album to the library array 
     function addAlbumToLibrary(){
-        bookName = document.getElementById('bookName').value;
-        myLibrary.push(bookName);
+
+        //saving all user inputs into variables
+        var albumInput = document.getElementById('albumName').value;
+        var artistInput = document.getElementById('artist').value;
+        var songInput = document.getElementById('songs').value;
+        var listenedInput = document.getElementById('listened').value;
+
+        let album = new Album(albumInput, artistInput, songInput, listenedInput);
+        
+        console.log(album);
+
+        myLibrary.push(album);
+
         console.log(myLibrary);
 
-        return false;
+        
     }
+
+
