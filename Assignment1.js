e37a31937a473d6a11a82045b5ab789b08261f8b
estefaniamelo <melojoa@sheridancollege.ca>
2021-11-14 21:52:08 -0500

    Finished displayLibrary function. Added a modal in the HTML to display new album form and added CSS style.

--- Assignment1.js
+++ Assignment1.js
@@ -6,11 +6,21 @@
  * Your code should look something like this:
  */
     //declaring array
-    let myLibrary = [];
+    let myLibrary = [
+        {
+            albumName: "abc",
+            artist: "abc",
+            songs: "12",
+            listenedStatus: "yes"
+        },
+        {
+            albumName: "bcd",
+            artist: "bcd",
+            songs: "13",
+            listenedStatus: "bcd"
+        }
 
-
-    //adding event listener to addAlbum button
-   
+    ];
 
     function Album(albumName, artist, songs, listenedStatus){
         this.albumName = albumName;
@@ -37,6 +47,43 @@
         myLibrary.push(album);
 
         console.log(myLibrary);
-
-        
     }
+
+    document.getElementById("displayAlbum").addEventListener("click", displayLibrary);
+
+    function displayLibrary(){
+        var arrayLength = myLibrary.length;
+        let html = "";
+
+        for(var i=0; i < arrayLength; i++){
+
+            html += "<tr id=\"" + "album" + i + "\" + data-album-id=\"" + i + "\">" +
+            "<td>" + myLibrary[i].albumName + "</td>" +
+            "<td>" + myLibrary[i].artist + "</td>" +
+            "<td>" + myLibrary[i].songs + "</td>" +
+            "<td>" + myLibrary[i].listenedStatus + "</td>" + "<td>" + "<button type=\"button\"> Delete </button>"    
+            + "</tr>";
+
+
+        }
+
+        document.getElementById("tableBody").innerHTML = html;
+    }
+
+
+
+var btn = document.getElementById("add-btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+var modal = document.getElementById("album-modal");
+
+btn.onclick = function(){
+    console.log("in the function");
+    modal.style.display = "block";
+}
+
+span.onclick = function(){
+    console.log("in the function");
+    modalf.style.display = "none";
+}
